e202506f1220d4cc1935db2eebb89a6f75e46a8f
Yannick VM <yannickb@dsagency.fr>
2017-06-01 21:09:57 +0200

    first commit

---
+++ Examples/KdsRootKey_Example.ps1
@@ -0,0 +1,12 @@
+﻿Configuration Example {
+    Import-DscResource -ModuleName cActiveDirectory
+
+    node localhost {
+        KdsRootKey AD {
+            Ensure = "Present"
+        }
+
+    }
+}
+Example -OutputPath $env:TEMP
+Start-DscConfiguration -ComputerName localhost -Path $env:TEMP -Wait -Verbose
